bf74ae82453fa1b23ebbe348261020faad9227ef
Alex Ketavongsa <aktis@rit.edu>
2022-03-25 22:00:12 -0400

    changed the UI a bit to match our project

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -12,29 +12,9 @@ export default function Navbar() {
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <NavLink className="navbar-brand" to="/">
-        <img style={{"width" : 25 + '%'}} src="https://d3cy9zhslanhfa.cloudfront.net/media/3800C044-6298-4575-A05D5C6B7623EE37/4B45D0EC-3482-4759-82DA37D8EA07D229/webimage-8A27671A-8A53-45DC-89D7BF8537F15A0D.png"></img>
+        <img style={{"width" : 25 + '%'}} src="https://www.ny.gov/sites/default/files/thumbnails/image/NYGovFacebookMeta01LightGray.png"></img>
         </NavLink>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/create">
-                Create Record
-              </NavLink>
-            </li>
-          </ul>
-        </div>
+        <h3>NY State Parks Annual Attendance By Year</h3>
       </nav>
     </div>
   );
